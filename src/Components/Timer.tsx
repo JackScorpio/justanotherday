ac98d34a3c9c9482c816622e018266112b47dc20
Jack Scorpio <zjztom@gmail.com>
2021-04-13 13:19:15 +1000

    project started

---
+++ src/Components/Timer.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+interface Props {
+  timer: {
+    initialMinute: number;
+    initialSeconds: number;
+  };
+}
+
+const Timer: React.FC = () => {
+  const;
+};
+
+export default Timer;
