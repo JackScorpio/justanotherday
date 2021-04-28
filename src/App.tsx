49d602a8e074fffb6b9781d1af72dd28df9ceeac
Jack Scorpio <zjztom@gmail.com>
2021-04-28 22:13:12 +1000

    Clock component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
 import Clock from "./Components/Clock";
-import Timer from "./Components/Timer";
-import TodoList from "./Components/TodoList";
 
 function App() {
   // Notification.requestPermission();
@@ -11,64 +8,7 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='ui stackable three column padded grid headerBar'>
-        <div className='column'>
-          <Clock />
-        </div>
-        <div className='column startContainer'>
-          {!taskInit && (
-            <button
-              className='ui big green button'
-              onClick={() => setTaskInit(true)}
-            >
-              Start
-            </button>
-          )}
-          {taskInit && (
-            <button
-              className='ui big red button'
-              onClick={() => setTaskInit(false)}
-            >
-              End
-            </button>
-          )}
-          {taskInit && (
-            <div className='timer'>
-              <Timer />
-            </div>
-          )}
-        </div>
-        <div className='column widgetsContainer'>
-          <div className='ui compact menu'>
-            <a className='item'>
-              <i className='globe icon'></i>
-              Translate
-            </a>
-            <a className='item'>
-              <i className='video camera icon'></i>
-              Channels
-            </a>
-            <a className='ui pointing dropdown link item'>
-              <i className='external square alternate icon'></i>
-              Links
-              <i className='dropdown icon'></i>
-              <div className='menu'>
-                <div className='item'>
-                  <i className='dropdown icon'></i>
-                  <span className='text'>Categories</span>
-                  <div className='menu'>
-                    <div className='item'>Unread</div>
-                    <div className='item'>Promotions</div>
-                    <div className='item'>Updates</div>
-                  </div>
-                </div>
-                <div className='item'>Archive</div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <TodoList />
+      <Clock />
     </div>
   );
 }
--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import "./Clock.css";
 function Clock() {
-  return <div className='clock'></div>;
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString()
+  );
+
+  useEffect(() => {
+    setInterval(() => setCurrentTime(new Date().toLocaleTimeString()), 1000);
+  }, [currentTime]);
+
+  return <div className='clock'>{currentTime}</div>;
 }
 
 export default Clock;
