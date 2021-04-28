f5e520423d3f9c47264a185c8f0796b9a5c81b32
Jack Scorpio <zjztom@gmail.com>
2021-04-28 22:41:33 +1000

    add timer component  and function buttons on main header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import "./App.css";
 import React, { useState } from "react";
-
+import Timer from "./Components/Timer";
 import Clock from "./Components/Clock";
 
 function App() {
@@ -8,7 +9,63 @@ function App() {
 
   return (
     <div className='App'>
-      <Clock />
+      <div className='ui stackable three column padded grid headerBar'>
+        <div className='column'>
+          <Clock />
+        </div>
+        <div className='column startContainer'>
+          {!taskInit && (
+            <button
+              className='ui big green button'
+              onClick={() => setTaskInit(true)}
+            >
+              Start
+            </button>
+          )}
+          {taskInit && (
+            <button
+              className='ui big red button'
+              onClick={() => setTaskInit(false)}
+            >
+              End
+            </button>
+          )}
+          {taskInit && (
+            <div className='timer'>
+              <Timer />
+            </div>
+          )}
+        </div>
+        <div className='column widgetsContainer'>
+          <div className='ui compact menu'>
+            <a className='item'>
+              <i className='globe icon'></i>
+              Translate
+            </a>
+            <a className='item'>
+              <i className='video camera icon'></i>
+              Channels
+            </a>
+            <a className='ui pointing dropdown link item'>
+              <i className='external square alternate icon'></i>
+              Links
+              <i className='dropdown icon'></i>
+              <div className='menu'>
+                <div className='item'>
+                  <i className='dropdown icon'></i>
+                  <span className='text'>Categories</span>
+                  <div className='menu'>
+                    <div className='item'>Unread</div>
+                    <div className='item'>Promotions</div>
+                    <div className='item'>Updates</div>
+                  </div>
+                </div>
+                <div className='item'>Archive</div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Timer.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  let initialMinute = 45;
+  let initialSeconds = 0;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+          let notice = new Notification("Time for a break and drink water!", {
+            silent: false,
+            renotify: false,
+            requireInteraction: true,
+          });
+          notice.onclick = function () {
+            window.focus();
+            setMinutes(45);
+          };
+          notice.onclose = function () {
+            setMinutes(45);
+          };
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
+  return (
+    <div className='message'>
+      {minutes === 0 && seconds === 0 ? null : (
+        <h1>
+          Take a rest in: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default Timer;
