49f3ad74151b63caa5dd79b6373f3c06e07d601b
JacoScorpio <zjztom@gmail.com>
2021-05-30 15:02:27 +1000

    all

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -10,35 +10,38 @@ const Timer = () => {
   const [minutes, setMinutes] = useState(initialMinute);
   const [seconds, setSeconds] = useState(initialSeconds);
   useEffect(() => {
-    let myInterval = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-      }
-      if (seconds === 0) {
-        if (minutes === 0) {
-          clearInterval(myInterval);
-          let notice = new Notification("Time for a break and drink water!", {
-            silent: false,
-            renotify: false,
-            requireInteraction: true,
-          });
-          notice.onclick = function () {
-            window.focus();
-            setMinutes(45);
-          };
-          notice.onclose = function () {
-            setMinutes(45);
-          };
-        } else {
-          setMinutes(minutes - 1);
-          setSeconds(59);
+    if (taskInit) {
+      let myInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
         }
-      }
-    }, 1000);
-    return () => {
-      clearInterval(myInterval);
-    };
-  }, [seconds, minutes]);
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(myInterval);
+            let notice = new Notification("Time for a break and drink water!", {
+              silent: false,
+              renotify: false,
+              requireInteraction: true,
+            });
+            notice.onclick = function () {
+              window.focus();
+              setMinutes(45);
+            };
+            notice.onclose = function () {
+              setMinutes(45);
+            };
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+      return () => {
+        clearInterval(myInterval);
+      };
+    }
+    
+  }, [taskInit,seconds]);
 
   return (
     <div className='column startContainer'>
